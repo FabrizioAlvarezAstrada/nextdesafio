85452cdeebdbf26400d435538ae64bec27c36582
Fabrizio <fabriziopitu2018@gmail.com>
2024-10-03 15:36:18 -0300

    Lista de tarefas completa

--- src/app/ListaTarefas/page.js
+++ src/app/ListaTarefas/page.js
@@ -6,61 +6,75 @@ import "../globals.css"
 
 
 export default function ListaTarefas() {
-  const [Editar, SetEditar] = useState([-1])
-  const [Tarefas, SetTarefas] = useState([])
-  const [Elements, SetElements] = useState([])
+  const [Tarefas, SetTarefas] = useState(["Fazer o CSS.", "Aprender CRUD.", "Passar de Ano.", "Não reprovar de Ano."])
+  const [EditValue, SetEditValue] = useState("")
   const [InputValue, SetInputValue] = useState("")
+  const [Editando, SetEditando] = useState(-1)
+  // Consts
+  
+  const [Render, SetRender] = useState(false) //Força o react a atualizar
 
+  //Salvar tarefa
   function add(event) {
     event.preventDefault()
+    if (InputValue.trim() === "") { return }
     Tarefas.push(InputValue)
-    SetTarefas(Tarefas)
 
     SetInputValue("")
-    render()
+    SetEditando(-1)
+    SetTarefas(Tarefas)
+    SetRender(!Render)
   }
 
+  //Remover tarefa
   function remover(Index) {
     Tarefas.splice(Index, 1)
+
+    SetEditando(-1)
     SetTarefas(Tarefas)
-    render()
+    SetRender(!Render)
   }
 
+  //Mover tarefa
   function moverTarefa(Index, Dir) {
     const PoseNova = Tarefas[Index + Dir]
     const CurPos = Tarefas[Index]
     if (PoseNova == undefined) return
 
-    console.log(PoseNova)
-    console.log(CurPos)
-
     Tarefas[Index + Dir] = CurPos
     Tarefas[Index] = PoseNova
 
+    SetEditando(-1)
     SetTarefas(Tarefas)
-    render()
-}
-
-  function render() {
-    console.log(Tarefas)
-    SetElements(Tarefas.map((Tarefa, Index) => {
-      return (
-        <li id="Tarefa" key={Index}>
-          {Tarefa}
-
-          <div>
-            <button id="TarefaBotao" onClick={() => {remover(Index)}}>Remover</button>
-            <button id="TarefaBotao" onClick={() => {moverTarefa(Index, -1)}}>Subir</button>
-            <button id="TarefaBotao" onClick={() => {moverTarefa(Index, 1)}}>Descer</button>
-          </div>
-        </li>)
-    }))
+    SetRender(!Render)
   }
 
-  function getInput(event) {
+  //Editar tarefa
+  function editar(Index) {
+    SetEditValue(Tarefas[Index])
+    SetEditando(Index)
+    SetRender(!Render)
+  }
+
+  function salvarEdit() {
+    if (EditValue.trim() === "") { return }
+    Tarefas[Editando] = EditValue
+
+    SetEditando(-1)
+    SetTarefas(Tarefas)
+    SetRender(!Render)
+  }
+
+  //Inputs de texto
+  function getTaskInput(event) {
     SetInputValue(event.target.value)
   }
 
+  function getEditInput(event) {
+    SetEditValue(event.target.value)
+  }
+
+  //HTML Return
   return (
     <div id="MainBody">
       <LinkBar />
@@ -70,17 +84,41 @@ export default function ListaTarefas() {
           <h1 id="Titulo">Lista de Tarefas</h1>
 
           <form id="Controle" onSubmit={add} autoComplete="off">
-            <input id="Input" onChange={getInput} value={InputValue} />
+            <input id="Input" onChange={getTaskInput} value={InputValue} />
             <button type="submit" id="Botao">Adicionar</button>
           </form>
 
           <ul id="Lista">
-            {Elements}
+            {
+              //
+              Tarefas.map((Tarefa, Index) => {
+                if (Index == Editando) {
+                  return (
+                    <li id="Editando" key={Index}>
+                      <div id="EditandoMenu">
+                        <input id="Input" onChange={getEditInput} value={EditValue} autoComplete="off" />
+                        <button id="TarefaBotao" onClick={salvarEdit}>Salvar</button>
+                      </div>
+                    </li>)
+                }
+                //
+                return (
+                  <li id="Tarefa" key={Index}>
+                    {Tarefa}
+
+                    <div id="TarefaMenu">
+                      <button id="TarefaBotao" onClick={() => remover(Index)}>Remover</button>
+                      <button id="TarefaBotao" onClick={() => moverTarefa(Index, -1)}>Subir</button>
+                      <button id="TarefaBotao" onClick={() => moverTarefa(Index, 1)}>Descer</button>
+                      <button id="TarefaBotao" onClick={() => editar(Index)}>Editar</button>
+                    </div>
+                  </li>)
+              })
+              //
+            }
           </ul>
         </div>
       </div>
-
     </div>
-
   );
 }
